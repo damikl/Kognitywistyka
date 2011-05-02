8670786675cc9c9a22b71d96c7696a64d3234a3d
Michał Ładanowski <ladan8@gmail.com>
2011-05-02 02:20:34 +0200

    Kilka rzeczy o których zapomniałem
    Signed-off-by: Michał Ładanowski <ladan8@gmail.com>

--- src/main/java/pl/edu/uj/kognitywistyka/publication/bo/PublicationBoImpl.java
+++ src/main/java/pl/edu/uj/kognitywistyka/publication/bo/PublicationBoImpl.java
@@ -2,6 +2,13 @@ package pl.edu.uj.kognitywistyka.publication.bo;
 
 import java.io.Serializable;
 
-public class PublicationBoImpl implements PublicationBo, Serializable {
+import pl.edu.uj.kognitywistyka.publication.dao.PublicationDao;
 
+public class PublicationBoImpl implements PublicationBo, Serializable {
+	private static final long serialVersionUID = 1L;
+	PublicationDao publicationDao;
+
+	public void setPublicationDao(PublicationDao publicationDao) {
+		this.publicationDao = publicationDao;
+	}
 }
